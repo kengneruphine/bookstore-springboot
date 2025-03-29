9561ea87067e850d0de558b6fcdf2192d3a0cb42
kengneruphine <ruphinekengne@gmail.com>
2025-03-29 15:51:58 +0200

    feat: book entity and dto class

--- src/main/java/com/ruphine/bookstore/dto/BookDto.java
+++ src/main/java/com/ruphine/bookstore/dto/BookDto.java
@@ -1,2 +1,19 @@
-package com.ruphine.bookstore.dto;public class BookDto {
+package com.ruphine.bookstore.dto;
+
+import com.ruphine.bookstore.enums.BookGenre;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BookDto {
+    private Long id;
+    private String title;
+    private BookGenre genre;
+    private LocalDate publishDate;
 }
--- src/main/java/com/ruphine/bookstore/entities/Book.java
+++ src/main/java/com/ruphine/bookstore/entities/Book.java
@@ -1,2 +1,29 @@
-package com.ruphine.bookstore.entities;public class Book {
+package com.ruphine.bookstore.entities;
+
+import com.ruphine.bookstore.enums.BookGenre;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    @Column(name="publish_date")
+    private LocalDate publishDate;
+
+    private BookGenre genre;
+
+
+
 }

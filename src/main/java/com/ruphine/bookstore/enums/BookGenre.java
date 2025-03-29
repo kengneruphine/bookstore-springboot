bdb0ee67e47e09c16eb2d8da916250cd78022e17
kengneruphine <ruphinekengne@gmail.com>
2025-03-29 15:51:07 +0200

    feat: bookgenre class and its serialzation methods

--- src/main/java/com/ruphine/bookstore/enums/BookGenre.java
+++ src/main/java/com/ruphine/bookstore/enums/BookGenre.java
@@ -1,2 +1,37 @@
-package com.ruphine.bookstore.enums;public enum BookGenre {
+package com.ruphine.bookstore.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum BookGenre {
+    FICTION,
+    BIOGRAPHY,
+    FANTASY,
+    SCIENCE,
+    ROMANCE,
+    NON_FICTION,
+    MYSTERY,
+    UNKNOWN;
+
+
+    // This method is called when deserializing JSON to Enum
+    @JsonCreator
+    public static BookGenre fromValue(String value) {
+        if (value == null) {
+            return UNKNOWN;
+        }
+        for (BookGenre genre : values()) {
+            if (genre.name().equalsIgnoreCase(value)) {  // Case-insensitive check
+                return genre;
+            }
+        }
+        return UNKNOWN;
+    }
+
+    // This method is called when serializing Enum to JSON
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
+
 }

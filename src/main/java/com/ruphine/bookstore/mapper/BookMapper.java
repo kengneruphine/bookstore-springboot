12a2a417a14e60ae63fdeb13c9ef6f3d30b3b078
kengneruphine <ruphinekengne@gmail.com>
2025-03-29 15:52:39 +0200

    feat: entity to dto conversion and vice versa

--- src/main/java/com/ruphine/bookstore/mapper/BookMapper.java
+++ src/main/java/com/ruphine/bookstore/mapper/BookMapper.java
@@ -1,2 +1,14 @@
-package com.ruphine.bookstore.mapper;public class BookMapper {
+package com.ruphine.bookstore.mapper;
+
+import com.ruphine.bookstore.dto.BookDto;
+import com.ruphine.bookstore.entities.Book;
+
+public class BookMapper {
+    public static BookDto mapToBookDto(Book book){
+        return new BookDto(book.getId(), book.getTitle(), book.getGenre(), book.getPublishDate());
+    }
+
+    public static Book mapToBookEntity(BookDto bookDto){
+        return new Book(bookDto.getId(), bookDto.getTitle(), bookDto.getPublishDate(),bookDto.getGenre());
+    }
 }

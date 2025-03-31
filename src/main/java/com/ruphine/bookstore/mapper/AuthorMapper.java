23ec8a3f2a78911e2fef7a4d3b29216ec50c85a0
kengneruphine <ruphinekengne@gmail.com>
2025-03-31 22:32:58 +0200

    feat: adding the authorMapper class refactoring the bookMapper class

--- src/main/java/com/ruphine/bookstore/mapper/AuthorMapper.java
+++ src/main/java/com/ruphine/bookstore/mapper/AuthorMapper.java
@@ -1,2 +1,15 @@
-package com.ruphine.bookstore.mapper;public class AuthorMapper {
+package com.ruphine.bookstore.mapper;
+
+import com.ruphine.bookstore.dto.AuthorDto;
+import com.ruphine.bookstore.entities.Author;
+
+public class AuthorMapper {
+
+    public static AuthorDto mapToAuthorDto(Author author){
+        return new AuthorDto(author.getId(), author.getFirstName(), author.getLastName(), author.getEmail(), author.getBooks());
+    }
+
+    public static Author mapToAuthorEntity(AuthorDto authorDto){
+        return new Author(authorDto.getId(), authorDto.getFirstName(), authorDto.getLastName(), authorDto.getEmail(), authorDto.getBooks());
+    }
 }
--- src/main/java/com/ruphine/bookstore/mapper/BookMapper.java
+++ src/main/java/com/ruphine/bookstore/mapper/BookMapper.java
@@ -5,10 +5,10 @@ import com.ruphine.bookstore.entities.Book;
 
 public class BookMapper {
     public static BookDto mapToBookDto(Book book){
-        return new BookDto(book.getId(), book.getTitle(), book.getGenre(), book.getPublishDate());
+        return new BookDto(book.getId(), book.getTitle(), book.getGenre(), book.getPublishDate(), book.getAuthors());
     }
 
     public static Book mapToBookEntity(BookDto bookDto){
-        return new Book(bookDto.getId(), bookDto.getTitle(), bookDto.getPublishDate(),bookDto.getGenre());
+        return new Book(bookDto.getId(), bookDto.getTitle(), bookDto.getPublishDate(),bookDto.getGenre(), bookDto.getAuthors());
     }
 }

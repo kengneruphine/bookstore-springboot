228a62ce7514f5aae87b2ac7e9462b78b74c7d2a
kengneruphine <ruphinekengne@gmail.com>
2025-04-09 19:09:55 +0200

    feat: query to return all the authors who wrote a book

--- src/main/java/com/ruphine/bookstore/repositories/AuthorRepository.java
+++ src/main/java/com/ruphine/bookstore/repositories/AuthorRepository.java
@@ -1,2 +1,13 @@
-package com.ruphine.bookstore.repositories;public interface AuthorRepository {
+package com.ruphine.bookstore.repositories;
+
+import com.ruphine.bookstore.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query("SELECT a FROM Author a JOIN a.books b WHERE b.id= :bookId")
+    List<Author> findAuthorsByBookId(@Param("bookId") Long bookId);
 }
